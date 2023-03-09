4447d8621243526e3716891abdb123bf05fe7717
OmbuiMark <102910738+OmbuiMark@users.noreply.github.com>
2023-03-09 06:54:53 -0800

    removed action bars

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.e_commerce
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock.sleep
 import com.example.e_commerce.databinding.ActivityMainBinding
 
 
@@ -14,11 +13,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater) // Initialize view binding object
         setContentView(binding.root)
 
-
-
         binding.hamburgericon.setOnClickListener {
-        val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
