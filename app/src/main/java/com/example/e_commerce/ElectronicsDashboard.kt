c69453a4cd28b12ee9bdc9e3a011ad91abdc33af
OmbuiMark <102910738+OmbuiMark@users.noreply.github.com>
2023-03-22 11:21:34 -0700

    Update electronics dashboard
    The cart icon increments. But only in the GUI

--- app/src/main/java/com/example/e_commerce/ElectronicsDashboard.kt
+++ app/src/main/java/com/example/e_commerce/ElectronicsDashboard.kt
@@ -2,10 +2,24 @@ package com.example.e_commerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class ElectronicsDashboard : AppCompatActivity() {
+
+    private var cartCount = 0
+    private lateinit var cartCountTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_electronics_dashboard)
+
+        cartCountTextView = findViewById(R.id.cart_count)
+    }
+
+    fun addToCart(view: View) {
+        // Increment cart count and update TextView
+        cartCount++
+        cartCountTextView.text = cartCount.toString()
     }
 }
